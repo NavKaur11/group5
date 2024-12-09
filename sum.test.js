6329b69d77fd5e0c47bb0638c2bf1da109864489
Navneet Kaur <navkaur6239@gmail.com>
2024-12-09 16:19:07 -0500

    again checking test cases

--- sum.test.js
+++ sum.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 18 + 20 to equal 30', () => {
-  expect(sum(119, 20)).toBe(30);
+test('adds 10 + 20 to equal 30', () => {
+  expect(sum(10, 20)).toBe(30);
 });
